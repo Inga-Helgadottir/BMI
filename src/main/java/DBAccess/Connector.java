b5adad90b64778558d28a7579a3c99c6cc392239
Inga-Helgadottir <78899604+Inga-Helgadottir@users.noreply.github.com>
2021-11-06 14:57:21 +0100

    Update Connector.java

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -40,11 +40,8 @@ public class Connector {
         } else {
             // Localhost
             URL = "jdbc:mysql://localhost:3306/useradmin?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-//            USERNAME = "dat2user";
-//            PASSWORD = "password";
-//            URL = "jdbc:mysql://localhost:3306/Bibliotek?serverTimezone=CET&useSSL=false";
             USERNAME = "sih";
-            PASSWORD = "mysqlPasswordSIH995";
+            PASSWORD = "put your password here";
         }
     }
 
